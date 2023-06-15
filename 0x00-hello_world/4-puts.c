0673d8e19566a860a4c9799e8cd16090b45cf072
Mohamed Amr <momo.amr.f@gmail.cm>
2023-06-16 01:18:49 +0200

    a

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
-
+/**
+* main - main function of the program
+* Description: Longer description of the main function
+* Return: void
+*/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
