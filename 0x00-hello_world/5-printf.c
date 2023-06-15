ec9673b57e8d891121f92ae8cc8860b835181f34
Mohamed Amr <momo.amr.f@gmail.cm>
2023-06-16 01:20:05 +0200

    a

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+* main - main function of the program
+* Description: Longer description of the main function
+* Return: void
+*/
+int main(void)
+{
+	printf("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
+
