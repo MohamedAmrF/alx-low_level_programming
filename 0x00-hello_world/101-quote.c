7545eef76bf0715bd20e7e9bb823591f5b7b2169
Mohamed Amr <momo.amr.f@gmail.cm>
2023-06-16 02:50:22 +0200

    "a"

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+* main - main function of the program
+* Description: Longer description of the main function
+* Return: void
+*/
+int main(void)
+{
+	char q[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	
+	write(1.q.59);
+	return (1);
+}
+
