94b2f02b4ae76541c2a416e17bb65fd1a0cdf1f5
Mohamed Amr <momo.amr.f@gmail.cm>
2023-06-27 01:28:56 +0200

    a

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -21,7 +21,7 @@ int main(void)
 	if (d > 5)
 		printf("and is greater than 5");
 	else if (d == 0)
-		printf("and is zero");
+		printf("and is 0");
 	else
 		printf("and is less than 6 and not 0");
 	printf("\n");
