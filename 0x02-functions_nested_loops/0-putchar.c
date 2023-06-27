f54373e89066db3fc48242516c2f276062e32206
Mohamed Amr <momo.amr.f@gmail.cm>
2023-06-27 15:11:01 +0200

    a

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');	
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+}
