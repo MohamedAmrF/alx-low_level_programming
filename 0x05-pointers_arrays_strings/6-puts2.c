a41b65e7e9b426385bb9db71439e336d97d84b42
Mohamed Amr <momo.amr.f@gmail.cm>
2023-06-27 23:53:13 +0200

    a

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* puts2 - prints
+* @str: input
+*
+* Return: void
+*/
+void puts2(char *str)
+{
+	int i = 0;
+	while (*str != '\0')
+	{
+		if (i % 2 == 0)
+			_putchar(*str++);
+		else
+			*str++;
+	}
+}
