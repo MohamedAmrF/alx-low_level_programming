6de5d92e95e32b41bf214345a60ea44813ad3269
Mohamed Amr <momo.amr.f@gmail.cm>
2023-06-27 23:49:59 +0200

    a

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -12,12 +12,12 @@ void rev_string(char *s)
 	int n = strlen(s);
 
 	int l = 0;
-	int r = n-1;
+	int r = n - 1;
 
 	while (l < r)
 	{
 		int temp = s[l];
-		
+
 		s[l] = s[r];
 		s[r] = temp;
 		l++;
