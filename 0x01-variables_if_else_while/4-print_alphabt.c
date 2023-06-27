68a9446ff98563778ad87521d9fb28e97d3df64b
Mohamed Amr <momo.amr.f@gmail.cm>
2023-06-27 02:47:07 +0200

    a

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main - main function
+* Return: 0 (success)
+*/
+int main(void)
+{
+	char c;
+
+	c = 'a';
+	while (c <= 'z')
+	{
+		if (c == 'q' || c == 'e')
+			continue;
+		putchar(c++);
+	}
+	putchar('\n');
+
+	return (0);
+}
