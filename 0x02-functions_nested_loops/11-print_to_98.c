11a12d22ee47838b9a2e444231adc9a1241bf6f4
Mohamed Amr <momo.amr.f@gmail.cm>
2023-06-27 22:25:32 +0200

    a

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* print_to_98 - print number till they reach 98
+*@n: number
+*
+* Return: void (always)
+*/
+void print_to_98(int n)
+{
+	while (n != 98)
+	{
+		if (n > 98)
+			n--;
+		else
+			n++;
+		_putchar(n + '0');
+		_putchar(',');
+		_putchar(' ');
+	}
+	_putchar('0' + 98);
+}
