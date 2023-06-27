1eeb0bb2abe92d6ec2fcf7f79d0c3a6cf3f02c54
Mohamed Amr <momo.amr.f@gmail.cm>
2023-06-27 20:02:40 +0200

    a

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,10 +1,17 @@
 #include "main.h"
 
 /**
+* print_sign - check sign of number
+* @n: number to be checked
 *
-*
-*
-*
-*
+* Return: 1 (pos), -1 (neg), 0 (zero)
 */
-
+int print_sign(int n)
+{
+	if (n > 0)
+		return (1);
+	else if (n == 0)
+		return (0);
+	else
+		return (-1);
+}
