214fc948988b960730bcf28f0c87716bb93d7f1b
Mohamed Amr <momo.amr.f@gmail.cm>
2023-06-27 22:27:46 +0200

    a

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -14,9 +14,13 @@ void print_to_98(int n)
 			n--;
 		else
 			n++;
+		if (n >= 10)
+			_putchar('0' + (n / 10) % 10);
 		_putchar(n + '0');
 		_putchar(',');
 		_putchar(' ');
 	}
-	_putchar('0' + 98);
+	_putchar('9');
+	_putchar('8');
+	_putchar('\n');
 }
