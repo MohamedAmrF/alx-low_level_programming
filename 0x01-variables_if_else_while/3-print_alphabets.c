17586d92ca4f486e136890ddad82e5285078c1a6
Mohamed Amr <momo.amr.f@gmail.cm>
2023-06-27 02:43:17 +0200

    a

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main - main function
+* Return: 0 (success)
+*/
+int main(void)
+{
+	char c;
+
+	c = 'a';
+	while (c <= 'z')
+	{
+		putchar(c++);
+	}
+	c = 'A';
+	while(c <= 'Z')
+		putchar(c++);
+	putchar('\n');
+
+	return (0);
+}
