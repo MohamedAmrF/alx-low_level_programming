8b5092824a8cd935231c88efab925c78ecd91a3e
Mohamed Amr <momo.amr.f@gmail.cm>
2023-06-27 21:24:45 +0200

    a

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -15,7 +15,8 @@ void times_table(void)
 		{
 			int answer = i * j;
 
-			_putchar('0' + (answer / 10) % 10);
+			if (answer > 10)
+				_putchar('0' + (answer / 10) % 10);
 			_putchar('0' + answer % 10);
 			if (j < 9)
 			{
