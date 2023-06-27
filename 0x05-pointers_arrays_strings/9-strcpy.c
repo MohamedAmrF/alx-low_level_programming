8416aa05083fdbff8b1a95731ba39ba691de6165
Mohamed Amr <momo.amr.f@gmail.cm>
2023-06-28 00:31:52 +0200

    a

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+* _strcpy - copy src to dest
+* @dest: destination
+* @src: source
+*
+* Return: returns pointer to destination
+*/
+char *_strcpy(char *dest, char *src)
+{
+	char *temp_dest = dest;
+
+	while (*src != '\0')
+	{
+		*temp_dest = *src;
+		temp_dest++;
+		src++;
+	}
+	return (dest);
+}
