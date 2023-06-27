334c79a367fb5e2d37c5b3119a05a057aa62e260
Mohamed Amr <momo.amr.f@gmail.cm>
2023-06-27 02:49:28 +0200

    a

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main - main function
+* Return: 0 (success)
+*/
+int main(void)
+{
+	char c;
+
+	c = '0';
+	while (c <= '9')
+	{
+		putchar(c++);
+	}
+	putchar('\n');
+
+	return (0);
+}
