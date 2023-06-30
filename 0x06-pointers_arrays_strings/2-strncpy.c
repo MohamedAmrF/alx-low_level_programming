b620f2e0fa3faa5bfa1e1221dc3e5ff69719f394
Mohamed Amr <momo.amr.f@gmail.cm>
2023-06-30 04:02:25 +0200

    a

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+* _strncpy - copy
+* @dest: dest
+* @src: source
+* @n: number of chars
+*
+* Return: char pointer
+*/
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+	char *ret = dest;
+
+	for (i = 0; i < n && *src != '\0'; i++)
+	{
+		*dest = *src;
+	}
+	return (ret);
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -4,5 +4,5 @@
 #include <unistd.h>
 
 char *_strcat(char *dest, char *src);
-
+char *_strncpy(char *dest, char *src, int n);
 #endif
