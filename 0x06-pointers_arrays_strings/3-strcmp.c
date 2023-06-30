f2b1b91c70dbb08d60efb55ef60102ccbb9a679d
Mohamed Amr <momo.amr.f@gmail.cm>
2023-06-30 04:17:03 +0200

    a

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -12,9 +12,9 @@ int _strcmp(char *s1, char *s2)
 
 	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
 	{
-		if (s2[i] < s1[i])
+		if (s1[i] < s2[i])
 			return (-1);
-		else if (s2[i] >= s1[i])
+		else if (s1[i] > s2[i])
 			return (1);
 	}
 	return (ret);
