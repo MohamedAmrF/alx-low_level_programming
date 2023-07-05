d8930df491a7202c21e63244dad8db4b4bc53561
Mohamed Amr <momo.amr.f@gmail.cm>
2023-07-05 23:10:37 +0200

    a

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,25 +1,6 @@
 #include "main.h"
 
 /**
-* helper - asdf
-* @s1: sldkf
-* @s2: sldf
-* @f: asldf
-*
-* Return: int
-*/
-int helper (char *s1, char *s2, int f)
-{
-	if (*s1 == *s2 && *s1 == '\0')
-		return (1);
-        if (*s2 == '*')
-                return (helper(s1 + 1, s2 + 1, 1);
-	if (*s1 == *s2 || f)
-		return (helper(s1 + 1, s2 + 1, 0));
-	else
-		return (0);
-}
-/**
 * wildcmp - compare two strings
 * @s1: first string
 * @s2: second string
@@ -30,10 +11,13 @@ int wildcmp(char *s1, char *s2)
 {
 	if (*s1 == *s2 && *s1 == '\0')
 		return (1);
-	if (*s2 == '*')
-		return (helper(s1, s2, 0);
 	if (*s1 == *s2)
 		return (wildcmp(s1 + 1, s2 + 1));
-	else
-		return (0);
+	if (*s2 == '*')
+	{
+		if (*(s2 + 1) == '*')
+			return (wildcmp(s1, s2 + 1));
+
+		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
+	}
 }
