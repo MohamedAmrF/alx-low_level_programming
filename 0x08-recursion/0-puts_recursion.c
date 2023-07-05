c063b5e92142b78ab50ecd751de081291b7302b7
Mohamed Amr <momo.amr.f@gmail.cm>
2023-07-05 18:41:13 +0200

    a

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* _puts_recursion - prints string
+* @s: parameter
+*
+* Return: void
+*/
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return;
+	}
+	_puts(*s);
+	_puts_recursion(s + 1);
+}
+
