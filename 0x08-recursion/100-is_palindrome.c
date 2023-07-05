611f62436d0ad97973e75b2c8ca67425c06ceb7b
Mohamed Amr <momo.amr.f@gmail.cm>
2023-07-05 22:44:37 +0200

    a

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include "string.h"
+
+/**
+* helper - helper func
+* @s: sdlf
+* @l: left pointer
+* @r: right pointer
+*
+* Return: int
+*/
+int helper (char *s, int l, int r)
+{
+	if (l >= r)
+		return (1);
+	if (s[l] == s[r])
+	{
+		return (helper(s, l+1, r-1));
+	}
+	else
+		return (0);
+}
+
+/**
+* is_palidrome - check if palindrome
+* @s: input string
+*
+* Return: 1 if palindrome 0 otherwise
+*/
+int is_palindrome(char *s)
+{
+	return (helper(s, 0, strlen(s)-1));
+}
