6c8406c4fbad0ea025419c5e9fa2d82d8651fe43
Mohamed Amr <momo.amr.f@gmail.cm>
2023-07-05 19:56:46 +0200

    a

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* factorial - asdf
+* @n: paramter
+*
+* Return: factorial of a number
+*/
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+
+	if (n <= 1)
+	{
+		return (1);
+	}
+	return (n * factorial(n-1));
+}
