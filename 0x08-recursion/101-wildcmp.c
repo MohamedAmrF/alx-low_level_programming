7cb1381047737abfa2df44956795fbbb4b109342
Mohamed Amr <momo.amr.f@gmail.cm>
2023-07-05 22:49:57 +0200

    a

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* wildcmp - compare two strings
+* @s1: first string
+* @s2: second string
+*
+* Return: 1 if equal 0 otherwise
+*/
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == *s2 && *s1 = '\0')
+		return (1);
+	if (*s1 == *s2)
+		return (wildcmp(s1+1, s2+1));
+	else
+		return (0);
+}
