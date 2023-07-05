b20c1af1a4ceda3e829ad39218f1574948bf1dcd
Mohamed Amr <momo.amr.f@gmail.cm>
2023-07-05 19:21:30 +0200

    a

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -12,7 +12,7 @@ void _puts_recursion(char *s)
 	{
 		return;
 	}
-	_puts(*s);
+	_putchar(*s);
 	_puts_recursion(s + 1);
 }
 
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -2,6 +2,7 @@
 #define MAIN
 
 #include <unistd.h>
+int _putchar(char c);
 void _puts_recursion(char *s);
 
 
