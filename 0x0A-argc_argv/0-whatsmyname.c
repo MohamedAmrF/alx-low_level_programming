e2ef42582ca6e9c97bef9fa647380e6b4d8df972
Mohamed Amr <momo.amr.f@gmail.cm>
2023-07-07 16:12:16 +0200

    a

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+* main - entry function
+* @argc: argument count
+* @argv: argument vector
+*
+* Return: int (0) in case of success
+*/
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	exit(EXIT_SUCCESS);
+}
---
+++ 0x0A-argc_argv/REAME.md
@@ -0,0 +1,1 @@
+practive on argv and argc
