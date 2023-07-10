0adfae1634248c1ca078309434c8e919c6280b65
Mohamed Amr <momo.amr.f@gmail.cm>
2023-07-10 15:18:47 +0200

    added 2-

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+* str_concat - concatenate
+* @s1: string 1
+* @s2: string 2
+*
+* Return: base address on new array
+*/
+char *str_concat(char *s1, char *s2)
+{
+	int i;
+	int n;
+	char * arr;
+
+	if (!s1 || !s2)
+		return (0);
+
+	n = strlen(s1) + strlen(s2) + 1;
+	arr = malloc(n);
+	if (!arr)
+		return (0);
+
+	for (i = 0; i < n; i++)
+	{
+		if (i < strlen(s1)
+			arr[i] = s1[i];
+		else
+			arr[i] = s2[i];
+	}
+
+	return ((char *)arr);
+}
