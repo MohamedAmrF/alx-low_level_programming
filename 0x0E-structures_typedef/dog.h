77babcf9d8b8fe736590f931a26e54b65ddfc5b7
Mohamed Amr <momo.amr.f@gmail.cm>
2023-07-17 11:32:00 +0200

    'a'

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,16 @@
 #ifndef DOGH
 #define DOGH
 
-struct dog {
+/**
+* struct dog - dog struct
+* @name: dog name
+* @age: dog age
+* @owner: owner name
+*
+* Description: long description
+*/
+struct dog
+{
 	char *name;
 	float age;
 	char *owner;
