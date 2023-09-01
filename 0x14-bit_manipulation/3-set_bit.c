2c8d08cf5cb72f70b955cff02fe796b820756cfa
Mohamed Amr <momo.amr.f@gmail.cm>
2023-09-01 17:50:23 +0200

    added 3-

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* set_bit - set the bit
+* @n: number to be set in
+* @index: index of the bit to be set.
+*
+* Return: 1 if success 0 otherwise.
+*/
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (index >= 32)
+		return (-1);
+
+	n |= (1 << index);
+	return (1);
+}
