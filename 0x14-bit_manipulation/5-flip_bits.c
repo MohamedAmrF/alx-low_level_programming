374ad7580f8cd8963fbbaa567f09e887d3992323
Mohamed Amr <momo.amr.f@gmail.cm>
2023-09-01 18:12:28 +0200

    betty compliant file 5-

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-* int flip_bits - count number of bits needed to be flipped to let n=m
+* flip_bits - count number of bits needed to be flipped to let n=m
 * @n: starting number.
 * @m: ending number.
 *
@@ -11,9 +11,9 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 	int i = 63, ret = 0;
 
-	for(; i >= 0; i--)
+	for (; i >= 0; i--)
 	{
-		if(((n>>i)&1) != ((m>>i)&1))
+		if (((n >> i) & 1) != ((m >> i) & 1))
 			ret++;
 	}
 	return (ret);
