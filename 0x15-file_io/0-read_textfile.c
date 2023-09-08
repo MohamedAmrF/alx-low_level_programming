3318c15d7f9945855201c3fcafc640ae489ea3f4
Mohamed Amr <momo.amr.f@gmail.cm>
2023-09-08 16:00:30 +0200

    added file 0

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+* read_textfile - reads text from file and prints it.
+* @filename: name of file
+* @letters: number of bytes to read
+*
+* Return: number of bytes read and printed.
+*/
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int filedes;
+	ssize_t b;
+	char buff[READ_BUF_SIZE * 8];
+
+	if (!filename || !letters)
+		return (0);
+
+	filedes = open(filename, O_RDONLY);
+	if (filedes == -1)
+		return (0);
+	b = read(fd, &buff[0], letters);
+	b = write(STDOUT_FILENO, &buf[0], bytes);
+	close(filedes);
+	return (bytes);
+}
