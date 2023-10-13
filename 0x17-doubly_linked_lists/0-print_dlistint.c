37904ab529e51018de30f5f06089d80e8201e2c7
Mohamed Amr <momo.amr.f@gmail.cm>
2023-10-13 18:48:28 +0200

    a

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -10,14 +10,14 @@
 size_t print_dlistint(const dlistint_t *h)
 {
 	size_t ret = 0;
-	dlistint_t current = head;
+	dlistint_t* current = (dlistint_t* )h;
 
 	if (current == NULL)
 		return (ret);
 
-	while (current->next != NULL)
+	while (current!= NULL)
 	{
-		prinf("%d\n", current->n);
+		printf("%d\n", current->n);
 		current = current->next;
 		ret++;
 	}
